858d079c6016eb457993e154ca46e9ee45d6e312
syntacticNaCl <garrettrappaport@gmail.com>
2017-03-31 00:16:14 -0400

    move footer and nav to own files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,81 +21,17 @@
     </script>
 </head>
 <body>
+
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+      @include('layouts.nav')
+        
+      @yield('content')
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+      @include('layouts.footer')
 
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        @if (Auth::guest())
-                            <li>
-                                <a href="{{ url('https://github.com/syntacticnacl/budgeit') }}">Documentation</a>
-                            </li>
-                        @else
-                            <li>
-                                <a href="{{ url('/budget') }}">Budget</a>
-                            </li>
-                            <li>
-                                <a href="{{ url('/Overview') }}">Overview</a>
-                            </li>
-                            <li>
-                                <a href="{{ url('/transactions') }}">Transactions</a>
-                            </li>
-                        @endif
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
     </div>
-
+      
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="container text-center">
+
+        <div class="col-sm-12 col-md-8 col-md-offset-2">
+
+         &copy;2017 Budgeit
+
+        </div>
+
+</footer>
