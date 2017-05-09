6a57b74cf4b8a448a99db29bbf83ee5c273f5c7a
syntacticNaCl <garrettrappaport@gmail.com>
2017-05-08 21:38:16 -0400

    added some base budget item query scopes

--- app/BudgetItem.php
+++ app/BudgetItem.php
@@ -15,4 +15,54 @@ class BudgetItem extends Model
     public function groups() {
         return $this->belongsToMany(BudgetGroup::class);
     }
+
+    /**
+     * Scope a query to only include unallocated items.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUnallocated($query) {
+        return $query->expense()->where('status', 'unallocated');
+    }
+
+    /**
+     * Scope a query to only include allocated items.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAllocated($query) {
+        return $query->expense()->where('status', 'allocated');
+    }
+
+    /**
+     * Scope a query to only include paid items.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePaid($query) {
+        return $query->expense()->where('status', 'paid');
+    }
+
+    /**
+     * Scope a query to only include expenses.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeExpense($query) {
+        return $query->where('type', 'expense');
+    }
+
+    /**
+     * Scope a query to only include income.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeIncome($query) {
+        return $query->where('type', 'income');
+    }
 }
